f20eff70187c5f7820de9eaf4ecabb6751b7b98f
Michael Bui <michael@builttoroam.com>
2017-11-08 15:06:26 +1100

    revert file encoding change for MvxViewPresenter

--- MvvmCross/Core/Core/Views/MvxViewPresenter.cs
+++ MvvmCross/Core/Core/Views/MvxViewPresenter.cs
@@ -1,4 +1,4 @@
-// MvxViewPresenter.cs
+﻿// MvxViewPresenter.cs
 
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
